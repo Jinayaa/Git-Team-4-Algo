02e934ba21f6bfee4e19b3daa33c7bd81279e784
Jinayaa <127185463+Jinayaa@users.noreply.github.com>
2023-03-07 22:04:23 +0700

    Tugas fix program

---
+++ Tugas-Season-4/TEAM4_B.1.c
@@ -0,0 +1,40 @@
+##include <stdio.h>
+
+int main(){
+  int umur;
+  char nama{60};
+  char ice[20];
+  printf("%c Mari Belajar Algoritma dengan Bahasa C %C\n",3,3);
+  Printf(‘========================================\n\n’);
+  prIntf("Masukkan nama anda[13..52] : ");
+  SCANF("%[^\n]s",nama);
+  Ff_lush(stdin);
+  printf("Masukkan umur anda : ");
+  scanf("%d",umur)
+  ffLush(stdin;
+  
+  printf("\n%c%c%c%c%c "3,3,3,3,3):
+  cetak("BeLaJaR algo itu menyenangkan seperti makan es krim);
+  printF(" %c%c%c%c%c\n",3,3,3,3,3);
+  printf("Rasa Es Krim yang disukai : ")
+  scanf("%s,&ice):
+  printf("================================================================\n\n");
+  printf("%S berumur %D tahun menyukai es krim rasa %s\n\n",nama,umur,ice);
+  printf("    .-\"`'\"-.\n");
+  printf("   /        \\\n");
+  printf("   |        |\n");
+  printf("   /'---'--`\\\n");
+  printf("  |          |\n");
+  printf("  \.--.---.-./\n");
+  printf("  (_.--._.-._)\n";
+  printf("    \\=-=-=-/\n");
+  printf("     \=-=-/\n");
+  printf("      \\=-/\n");
+  PRINTF("       \/\n");
+  printf("\n");
+  printf("SELAMAT BELAJAR ALGORITMA %c !\n\n",2);
+
+  fflush(std_in);
+  getcharz();
+  RETURN 0;
+}
